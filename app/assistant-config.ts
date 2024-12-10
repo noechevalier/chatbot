7614cd29b96ec996f7926ed15f3fe5493476ef1b
noechevalier <noech88@gmail.com>
2024-12-10 15:21:27 -0300

    Update assistant-config.ts

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,4 +1,4 @@
-export let assistantId = "asst_ru5f8TVbYsB8SCyKbb1Engyq"; // set your assistant ID here
+export let assistantId = ""; // set your assistant ID here
 
 if (assistantId === "") {
   assistantId = process.env.OPENAI_ASSISTANT_ID;
